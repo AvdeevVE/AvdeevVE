a2fdc94d6db66a85aaa53929d74d4dbe206f7e75
AvdeevVE <tlev2721@gmail.com>
2025-04-29 10:08:49 +0500

    Add files via upload

---
+++ api/server.py
@@ -0,0 +1,21 @@
+from flask import Flask, Response, jsonify, json
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+@app.get("/")
+def index():
+    return "Hello world"
+@app.get("/api/us")
+def get_us():
+    us = [
+        {
+            "name": "Type You",
+            "description": "Tupo You!",
+            "date": "2025-03-26"
+        }
+    ]
+    return Response(json.dumps(us), content_type="application/json")
+def main():
+    app.run("localhost", 8010, True)
+if __name__ == "__main__":
+    main()
